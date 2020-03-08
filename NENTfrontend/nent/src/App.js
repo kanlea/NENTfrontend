210529147122ec07063df5d966bb8f72d2dd2806
kanlea <kanlea@student.chalmers.se>
2020-03-08 15:32:51 +0100

    add base react

--- NENTfrontend/nent/src/App.js
+++ NENTfrontend/nent/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const { getAll, getOne }= require('./db');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    console.log('data is here');
+
+    var cursor = getOne();
+    console.log(cursor);
+
+  return ( <div className = "App">
+    <header className = "App-header"></header>
+    <div className="box">
+    <h1>Restaurant API</h1> 
+    <p><br></br><br></br><br></br><br></br></p>
+
     </div>
+     </div>
   );
 }
 
